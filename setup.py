204d28fcc3913f8568d2b656ad3904e7e159776a
90houlaoheshang <907333918@qq.com>
2020-07-10 16:22:14 +0800

    添加库设置

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asyncUnittest",
+    version="1.0.0",
+    author="Antas",
+    author_email="",
+    description="Fully asynchronous unit test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/monk-after-90s/asyncUnittest.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
