d015286eb44dcf321a2940af3a0fdd3dd2165b8e
90houlaoheshang <907333918@qq.com>
2020-07-13 10:10:25 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="asyncUnittest",
-    version="1.0.5",
+    version="1.0.6",
     author="Antas",
     author_email="",
     description="Fully asynchronous unit test",
